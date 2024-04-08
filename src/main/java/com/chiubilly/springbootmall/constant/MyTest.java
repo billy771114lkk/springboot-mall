1fe7bb73365a7be920f617ee2ee17503eae1d623
billy <billy771114lkk@gmail.com>
2024-04-08 23:34:49 +0800

    fuction refactor category in enum

---
+++ src/main/java/com/chiubilly/springbootmall/constant/MyTest.java
@@ -0,0 +1,15 @@
+package com.chiubilly.springbootmall.constant;
+
+public class MyTest {
+
+//    public static void main(String[] args) {
+//        ProductCategory productCategory = ProductCategory.FOOD;
+//        String s = productCategory.name();
+//        System.out.println(s);
+//
+//
+//        String s2 = "CAR";
+//        ProductCategory productCategory2 = ProductCategory.valueOf(s2);
+//
+//    }
+}
